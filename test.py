3e7eb4902799c4ffa8d78597952b0a874a261f47
Daniil Belov <bunsanandme@yandex.ru>
2024-04-04 06:28:47 +0300

    Add new endpoints for work with users/documents

--- test.py
+++
@@ -1,4 +0,0 @@
-import json
-text = "{\"documentType_Name\": \"Полис ОМС\", \"sprTerr_id\": \"1075\", \"type\": \"ОМС\", \"series\": 12433, \"number\": \"0253310891000710\", \"smo_id\": \"8000229\", \"beginDate\": \"2021-12-14\", \"endDate\": None, \"orgDep_Name\": \"РЕСО МЕД\", \"formType_id\": \"3\", \"lpu_id\": \"150028\"}"
-
-print(json.loads)
